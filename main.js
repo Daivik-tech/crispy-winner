c5bd17f54992926db53b369d2c2d1936a12a2376
Daivik-tech <84041224+Daivik-tech@users.noreply.github.com>
2021-05-15 18:52:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function submit()  {  
+    var display_student_array=[];
+    for( var j=1;j<=4;j++ )
+    var name_of_the_student=document.getElementById("name_of_the_student"+j).value;
+console.log(name_of_the_student);
+name_of_the_student_array.push(name_of_the_student);
+}
+console.log(name_of_the_student_array)
+var length_of_name_of_students_array=name_of_students_array.length;
+
+for ( var k=0<length_of-name_of_students_array k++){
+    display_student_array.push("<h4>NAME-"+name_of_the-student_array[k]+<h4>)
+}
